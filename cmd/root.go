29001024e3e67682a78f9bc0c79f378b063e6ad1
Ross Edman <ross.edman@gmail.com>
2024-07-29 10:10:01 -0500

    Add Code

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,46 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
-// top level instantion of the CLI
+type TestResult struct {
+	Time    string
+	Action  string
+	Package string
+	Test    string
+	Elapsed string
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "showit",
 	Short: "showit is a simple example of a go test parser",
+	Run: func(cmd *cobra.Command, args []string) {
+		s := bufio.NewScanner(os.Stdin)
+		results := []TestResult{}
+		for s.Scan() {
+			result := TestResult{}
+			if err := json.Unmarshal([]byte(s.Text()), &result); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			results = append(results, result)
+		}
+		for _, r := range results {
+			if r.Action == "pass" {
+				fmt.Printf("%s %s\n", color.GreenString("✅ PASS"), r.Test)
+			}
+			if r.Action == "fail" {
+				fmt.Printf("%s %s\n", color.RedString("❌ FAIL"), r.Test)
+			}
+		}
+	},
 }
 
 func Execute(version string) {
